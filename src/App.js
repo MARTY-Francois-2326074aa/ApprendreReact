48c6080181c9bcaf41d9912c4fbdf1899e49b825
fivema <francois.marty357@gmail.com>
2025-03-13 17:28:30 +0100

    Gif de garen

--- src/App.js
+++ src/App.js
@@ -1,24 +1,31 @@
-import logo from './logo.svg';
+import logo from './garen.gif';
 import './App.css';
+
+function Header() {
+  return (
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <h1>Taches</h1>
+      </header>
+  );
+}
+
+function Footer() {
+  return (
+      <footer>
+        <p>Ajouter</p>
+        <p>Tache</p>
+        <p>Categorie</p>
+      </footer>
+  );
+}
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <>
+        <Header />
+        <Footer />
+      </>
   );
 }
 
